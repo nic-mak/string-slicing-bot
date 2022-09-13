1ba145edf2104c120e82e063ed8a1f7913c729b5
mhoihean <nicholas.mak@u.nus.edu>
2022-09-13 17:52:22 +0800

    Deployment as Telegram Bot
    Changed main.py to OOP style to facilitate deployment as Telegram bot.
    Created bot logic in bot.py.

--- main.py
+++ main.py
@@ -9,61 +9,64 @@ CONSTANT_STRING_LENGTH_MAX = 10
 EMPTY_STRING = "\'\'"
 
 
-def get_random_string_fixed_length(n):
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for _ in range(n))
-    return result_str
+class QuestionGenerator:
+    def __init__(self):
+        pass
 
+    @staticmethod
+    def get_random_string_fixed_length(n):
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for _ in range(n))
+        return result_str
 
-def get_random_string():
-    n = random.randint(CONSTANT_STRING_LENGTH_MIN, CONSTANT_STRING_LENGTH_MAX)
-    return get_random_string_fixed_length(n)
+    def get_random_string(self):
+        n = random.randint(CONSTANT_STRING_LENGTH_MIN, CONSTANT_STRING_LENGTH_MAX)
+        return self.get_random_string_fixed_length(n)
 
+    @staticmethod
+    def get_random_start_stop_step(s):
+        n = len(s)
+        i_min, i_max = -n, n - 1
+        result = [random.randint(i_min, i_max) for _ in range(3)]
+        result[-1] = random.randint(-3, 3)  # Restrict step to between -3 and 3
+        return result
 
-def get_random_start_stop_step(s):
-    n = len(s)
-    i_min, i_max = -n, n - 1
-    result = [random.randint(i_min, i_max) for _ in range(3)]
-    result[-1] = random.randint(-3, 3)  # Restrict step to between -3 and 3
-    return result
+    @staticmethod
+    def parse_question(random_string, start, stop=None, step=None):
+        if step is None and stop is None:
+            question_string = f"\'{random_string}\'[{start}]"
+            answer_string = random_string[start]
+        elif step is None:  # and stop is not None
+            question_string = f'\'{random_string}\'[{start}:{stop}]'
+            answer_string = random_string[start:stop]
+        else:
+            question_string = f'\'{random_string}\'[{start}:{stop}:{step}]'
+            answer_string = random_string[start:stop:step] if step != 0 else 'ValueError'
 
+        if not answer_string:
+            answer_string = EMPTY_STRING
+        return question_string, answer_string
 
-def parse_question(random_string, start, stop=None, step=None):
-    if step is None and stop is None:
-        question_string = f"\'{random_string}\'[{start}]"
-        answer_string = random_string[start]
-    elif step is None:  # and stop is not None
-        question_string = f'\'{random_string}\'[{start}:{stop}]'
-        answer_string = random_string[start:stop]
-    else:
-        question_string = f'\'{random_string}\'[{start}:{stop}:{step}]'
-        answer_string = random_string[start:stop:step] if step != 0 else 'ValueError'
+    def get_question(self):
+        random_string = self.get_random_string()
+        start, stop, step = self.get_random_start_stop_step(random_string)
 
-    if not answer_string:
-        answer_string = EMPTY_STRING
-    return question_string, answer_string
+        random_roll = random.randint(1, 10)
+        if random_roll <= 2:  # P(only start index) = 0.2
+            question_string, answer_string = self.parse_question(random_string, start)
+        elif random_roll <= 6:  # P(start:stop) = 0.4
+            question_string, answer_string = self.parse_question(random_string, start, stop)
+        else:  # P(start:stop:step) = 0.4
+            question_string, answer_string = self.parse_question(random_string, start, stop, step)
 
+        # Extra code to reduce chances of empty string (empirically found that it's empty most of the time)
+        if answer_string == EMPTY_STRING:
+            if 3 <= random_roll <= 5:  # i.e. only start, stop  # 3/4 chance to swap start and stop
+                question_string, answer_string = self.parse_question(random_string, stop, start)
+            elif 7 <= random_roll <= 9:  # 3/4 chance to swap start and stop
+                question_string, answer_string = self.parse_question(random_string, stop, start, step)
 
-def get_question():
-    random_string = get_random_string()
-    start, stop, step = get_random_start_stop_step(random_string)
-
-    random_roll = random.randint(1, 10)
-    if random_roll <= 2:  # P(only start index) = 0.2
-        question_string, answer_string = parse_question(random_string, start)
-    elif random_roll <= 6:  # P(start:stop) = 0.4
-        question_string, answer_string = parse_question(random_string, start, stop)
-    else:  # P(start:stop:step) = 0.4
-        question_string, answer_string = parse_question(random_string, start, stop, step)
-
-    # Extra code to reduce chances of empty string (empirically found that it's empty most of the time)
-    if answer_string == EMPTY_STRING:
-        if 3 <= random_roll <= 5:  # i.e. only start, stop  # 3/4 chance to swap start and stop
-            question_string, answer_string = parse_question(random_string, stop, start)
-        elif 7 <= random_roll <= 9:  # 3/4 chance to swap start and stop
-            question_string, answer_string = parse_question(random_string, stop, start, step)
-
-    return question_string, answer_string
+        return question_string, answer_string
 
 
 def resource_path(relative_path):
@@ -82,9 +85,10 @@ def main():
     print(''.join([line for line in f.readlines()]))
     print('Implemented by Nicholas Mak\n')
 
+    question_generator = QuestionGenerator()
     question_number = 1
     while True:
-        question_string, answer_string = get_question()
+        question_string, answer_string = question_generator.get_question()
         print(f'Question Number {question_number}')
         print(question_string)
         user_input = input('Input your answer: ')
